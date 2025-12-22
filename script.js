005b663de348ebc7bc755a985fc8c5bc0f1b4f1b
Musaddique Qazi <38559064+mfmqazi@users.noreply.github.com>
2025-12-22 13:56:20 -0700

    Add Virtues of Holy Qur'an content (35 hadith stories) and fix Ramadhan story preview

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function init() {
         renderBooks();
         renderStories(fazailData.stories);
+        populateBookFilter();
         populateChapterFilter();
         setupEventListeners();
         updateStats();
